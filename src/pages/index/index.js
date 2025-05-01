bd325b3a03d1181d523bcf4ecfb17cf2595b5beb
antonio63963 <shotvideo25@gmail.com>
2025-05-01 17:00:02 +0300

    changed faces +++

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,13 +2,13 @@ import "../../common/styles/index.css";
 import "./index.css";
 import "../../common/ui/popupPage/popupPage.css";
 
-//Math Grants
-import "../mathGrants/mathGrants.css";
-import "../mathGrants/mediaMathGrants.css";
-import "../mathGrants/mathGrants.js";
+// //Math Grants
+// import "../mathGrants/mathGrants.css";
+// import "../mathGrants/mediaMathGrants.css";
+// import "../mathGrants/mathGrants.js";
 
-//Math Curation
-import "../mathCuration/mathCuration.js";
+// //Math Curation
+// import "../mathCuration/mathCuration.js";
 
 // Science Grants
 import "../scienceGrants/scienceGrants.js";
